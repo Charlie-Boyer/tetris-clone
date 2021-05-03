542a9d6ebab94fb5a485a871f17797fa5d6a1c68
Charlie-Boyer <charlie.josuas@gmail.com>
2021-05-03 20:13:10 +0200

    add rotation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,126 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [board, setBoard] = useState(new Array(20).fill('0000000000'))
+  const [baseBoard, setBaseBoard] = useState(new Array(20).fill('0000000000'))
+  const [piece, setPiece] = useState([
+    { x: 4, y: 0 },
+    { x: 3, y: 0 },
+    { x: 4, y: 1 },
+    { x: 5, y: 0 }
+  ])
+
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+
+      const currPiece = [...piece]
+      currPiece.forEach((e) => {
+        e.y++
+      })
+
+      setPiece(currPiece)
+
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+
+  useEffect(() => {
+
+    const currBoard = [...baseBoard]
+
+    piece.forEach((e) => {
+      let cell = currBoard[e.y].split('')
+      cell[e.x] = 1
+      currBoard[e.y] = cell.join('')
+    })
+
+    setBoard(currBoard)
+
+  }, [piece])
+
+  useEffect(() => {
+
+    function handleKeyDown(e) {
+      if (e.code == "Space") {
+        const currPiece = [...piece]
+
+        for(let i = 0; i < currPiece.length; i++){
+          if(i == 0) {
+            continue
+          }
+          if(currPiece[i].y == currPiece[0].y) {
+            let tempX = currPiece[i].x - (currPiece[i].x - currPiece[0].x)
+            let tempY = currPiece[0].y - (currPiece[i].x - currPiece[0].x)
+
+            currPiece[i].x = tempX
+            currPiece[i].y = tempY
+            console.log( (currPiece[i].x - currPiece[0].x))
+          }
+
+          // 3 - 0 
+          // 4 - 1
+          // 5 - 0
+          // 4 - (-1)
+
+        }
+     
+        
+
+        setPiece(currPiece)
+      }
+
+      if (e.code == "ArrowDown") {
+        const currPiece = [...piece]
+        currPiece.forEach((e) => {
+          e.y++
+        })
+
+        setPiece(currPiece)
+      }
+
+      if (e.code == "ArrowLeft") {
+        const currPiece = [...piece]
+        currPiece.forEach((e) => {
+          e.x--
+        })
+
+        setPiece(currPiece)
+      }
+
+      if (e.code == "ArrowRight") {
+        const currPiece = [...piece]
+        currPiece.forEach((e) => {
+          e.x++
+        })
+
+        setPiece(currPiece)
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onKeyDown={(e) => {
+      console.log(e.code == 'ArrowUp')
+    }}>
+      {board.map((cell) => (
+        <div className="row">
+          {cell.split('').map((e) => <div className={e == 1 ? "cell on" : "cell"}></div>)}
+        </div>
+      ))}
     </div>
   );
 }
---
+++ src/Cell.js
@@ -0,0 +1,18 @@
+import {useState} from 'react';
+
+
+export default function Cell({className, data}) {
+
+
+
+  return (
+    <div className={className}>
+      {/* <p>
+      {className}
+  
+
+      </p> */}
+      
+    </div>
+  )
+}
