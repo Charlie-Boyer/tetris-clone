e2621335c6077fc14aa029d37d2c743c1420ab3c
Charlie-Boyer <charlie.josuas@gmail.com>
2021-05-05 08:45:21 +0200

    refacto

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import { useEffect, useState } from 'react';
 function App() {
 
   const [board, setBoard] = useState(new Array(20).fill('0000000000'))
+  const [piece, setPiece] = useState(newPiece)
   const [baseBoard, setBaseBoard] = useState(new Array(20).fill('0000000000'))
+  const [land, setLand] = useState(1)
 
   function newPiece() {
     let key = Math.floor(Math.random() * 5)
@@ -49,129 +51,236 @@ function App() {
     }
   }
 
-  const [piece, setPiece] = useState(newPiece)
+  function handleKeyDown(e) {
 
 
+    // rotation counter clockwise
+    if (e.code == "Space") {
+      const currPiece = [...piece]
+      const newPiece = [...piece]
 
-  
+      for (let i = 0; i < currPiece.length; i++) {
 
-  useEffect(() => {
-    const interval = setInterval(() => {
+        // rotation center
+        if (i == 0) {
+          continue
+        }
 
+        // parts to rotate
+        let x = currPiece[0].x + (currPiece[i].y - currPiece[0].y)
+        let y = currPiece[0].y - (currPiece[i].x - currPiece[0].x)
+        newPiece[i].x = x
+        newPiece[i].y = y
+      }
+      setPiece(currPiece)
+    }
+
+    if (e.code == "ArrowDown") {
       const currPiece = [...piece]
       currPiece.forEach((e) => {
         e.y++
       })
 
       setPiece(currPiece)
+    }
 
-    }, 1000);
-    return () => clearInterval(interval);
-  }, [baseBoard]);
-
+    if (e.code == "ArrowLeft") {
 
 
-  
+      const currPiece = [...piece]
 
+      if (!currPiece.find(e => e.x == 0)) {
+        currPiece.forEach((e) => {
+          e.x--
+        })
+      }
 
+      setPiece(currPiece)
+    }
+
+    if (e.code == "ArrowRight") {
+      const currPiece = [...piece]
+
+      if (!currPiece.find(e => e.x == 9)) {
+        currPiece.forEach((e) => {
+          e.x++
+        })
+      }
+      setPiece(currPiece)
+    }
+  }
 
 
   useEffect(() => {
 
     const currBoard = [...baseBoard]
 
-
-
-    if (piece.find(e => e.y >= 20) || piece.find(e => currBoard[e.y][e.x] == 1) ) {
+    
+    if (piece.find(e => e.y >= 19) || piece.find(e => currBoard[e.y][e.x] == 1)) {
       setPiece(newPiece)
       setBaseBoard(board)
-
+      setLand(land + 1)
     }
-
-    else {
-      piece.forEach((e) => {
-        let cell = currBoard[e.y].split('')
-        cell[e.x] = 1
-        currBoard[e.y] = cell.join('')
-      })
-      setBoard(currBoard)
-    }
-
+    
+    piece.forEach((e) => {
+      let cell = currBoard[e.y].split('')
+      cell[e.x] = 1
+      currBoard[e.y] = cell.join('')
+    })
+    
+    setBoard(currBoard)
+    
   }, [piece])
 
-
-
-
-
-
-
-
+  
 
   useEffect(() => {
 
-    function handleKeyDown(e) {
+    console.log(land)
 
+    window.addEventListener('keydown', handleKeyDown)
 
-      // rotation counter clockwise
-      if (e.code == "Space") {
-        const currPiece = [...piece]
-        const newPiece = [...piece]
+    const interval = setInterval(() => {
+      const currPiece = [...piece]
+      currPiece.forEach((e) => {
+        e.y++
+      })
+      setPiece(currPiece)
+    }, 1000)
 
-        for (let i = 0; i < currPiece.length; i++) {
-
-          // rotation center
-          if (i == 0) {
-            continue
-          }
-
-          // parts to rotate
-          let x = currPiece[0].x + (currPiece[i].y - currPiece[0].y)
-          let y = currPiece[0].y - (currPiece[i].x - currPiece[0].x)
-          newPiece[i].x = x
-          newPiece[i].y = y
-        }
-        setPiece(currPiece)
-      }
-
-      if (e.code == "ArrowDown") {
-        const currPiece = [...piece]
-        currPiece.forEach((e) => {
-          e.y++
-        })
-
-        setPiece(currPiece)
-      }
-
-      if (e.code == "ArrowLeft") {
-
-
-        const currPiece = [...piece]
-
-        if (!currPiece.find(e => e.x == 0)) {
-          currPiece.forEach((e) => {
-            e.x--
-          })
-        }
-
-        setPiece(currPiece)
-      }
-
-      if (e.code == "ArrowRight") {
-        const currPiece = [...piece]
-
-        if (!currPiece.find(e => e.x == 9)) {
-          currPiece.forEach((e) => {
-            e.x++
-          })
-        }
-        setPiece(currPiece)
-      }
-    }
-    window.addEventListener('keydown', handleKeyDown);
     return () => {
+      clearInterval(interval);
       window.removeEventListener('keydown', handleKeyDown);
-    };
-  }, [piece]);
+    }
+
+  }, [land])
+
+  // const [baseBoard, setBaseBoard] = useState(new Array(20).fill('0000000000'))
+
+
+  // const [piece, setPiece] = useState(newPiece)
+
+
+
+
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+
+  //     const currPiece = [...piece]
+  //     currPiece.forEach((e) => {
+  //       e.y++
+  //     })
+
+  //     setPiece(currPiece)
+
+  //   }, 1000);
+  //   return () => clearInterval(interval);
+  // }, [baseBoard]);
+
+
+
+
+
+
+
+
+  // useEffect(() => {
+
+  //   const currBoard = [...baseBoard]
+
+
+
+  //   if (piece.find(e => e.y >= 20) || piece.find(e => currBoard[e.y][e.x] == 1)) {
+  //     setPiece(newPiece)
+  //     setBaseBoard(board)
+
+  //   }
+
+  //   else {
+  //     piece.forEach((e) => {
+  //       let cell = currBoard[e.y].split('')
+  //       cell[e.x] = 1
+  //       currBoard[e.y] = cell.join('')
+  //     })
+  //     setBoard(currBoard)
+  //   }
+
+  // }, [piece])
+
+
+
+
+
+
+
+
+
+  // useEffect(() => {
+
+  //   function handleKeyDown(e) {
+
+
+  //     // rotation counter clockwise
+  //     if (e.code == "Space") {
+  //       const currPiece = [...piece]
+  //       const newPiece = [...piece]
+
+  //       for (let i = 0; i < currPiece.length; i++) {
+
+  //         // rotation center
+  //         if (i == 0) {
+  //           continue
+  //         }
+
+  //         // parts to rotate
+  //         let x = currPiece[0].x + (currPiece[i].y - currPiece[0].y)
+  //         let y = currPiece[0].y - (currPiece[i].x - currPiece[0].x)
+  //         newPiece[i].x = x
+  //         newPiece[i].y = y
+  //       }
+  //       setPiece(currPiece)
+  //     }
+
+  //     if (e.code == "ArrowDown") {
+  //       const currPiece = [...piece]
+  //       currPiece.forEach((e) => {
+  //         e.y++
+  //       })
+
+  //       setPiece(currPiece)
+  //     }
+
+  //     if (e.code == "ArrowLeft") {
+
+
+  //       const currPiece = [...piece]
+
+  //       if (!currPiece.find(e => e.x == 0)) {
+  //         currPiece.forEach((e) => {
+  //           e.x--
+  //         })
+  //       }
+
+  //       setPiece(currPiece)
+  //     }
+
+  //     if (e.code == "ArrowRight") {
+  //       const currPiece = [...piece]
+
+  //       if (!currPiece.find(e => e.x == 9)) {
+  //         currPiece.forEach((e) => {
+  //           e.x++
+  //         })
+  //       }
+  //       setPiece(currPiece)
+  //     }
+  //   }
+  //   window.addEventListener('keydown', handleKeyDown);
+  //   return () => {
+  //     window.removeEventListener('keydown', handleKeyDown);
+  //   };
+  // }, [piece]);
 
 
 
