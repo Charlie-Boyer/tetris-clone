2e7a110aa724dca9921070acc18a92cd3ad8b3ee
Charlie-Boyer <charlie.josuas@gmail.com>
2021-05-12 22:19:00 +0200

    refacto with useReducer

--- src/App.js
+++ src/App.js
@@ -1,208 +1,215 @@
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useReducer, useCallback } from 'react';
+
 
 function App() {
 
   const [board, setBoard] = useState(new Array(20).fill('0000000000'))
-  const [piece, setPiece] = useState(newPiece)
   const [baseBoard, setBaseBoard] = useState(new Array(20).fill('0000000000'))
-  const [isLanded, setIsLanded] = useState(false)
+  const [emptyBoard, setEmptyBoard] = useState(new Array(20).fill('0000000000'))
   const [speed, setSpeed] = useState(1000)
-  const [hasLines, setHasLines] = useState(0)
 
-  function newPiece() {
-    let key = Math.floor(Math.random() * 5)
-    switch (key) {
-      case 0:
-        return [
-          { x: 4, y: 0 },
-          { x: 4, y: 1 },
-          { x: 5, y: 0 },
-          { x: 3, y: 1 }
-        ]
-      case 1:
-        return [
-          { x: 4, y: 0 },
-          { x: 5, y: 0 },
-          { x: 4, y: 1 },
-          { x: 5, y: 1 }
-        ]
-      case 2:
-        return [
-          { x: 4, y: 0 },
-          { x: 5, y: 0 },
-          { x: 3, y: 0 },
-          { x: 3, y: 1 }
-        ]
-      case 3:
-        return [
-          { x: 4, y: 0 },
-          { x: 5, y: 0 },
-          { x: 3, y: 0 },
-          { x: 4, y: 1 }
-        ]
-      case 4:
-        return [
-          { x: 4, y: 0 },
-          { x: 5, y: 0 },
-          { x: 3, y: 0 },
-          { x: 2, y: 0 }
-        ]
+  String.prototype.replaceAt = function (index, replacement) {
+    return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+  }
+
+  const pattern = [
+    ['0000', '0220', '0220', '0000'],
+    ['0000', '2222', '0000', '0000'],
+    ['002', '222', '000'],
+    ['200', '222', '000'],
+    ['022', '220', '000'],
+    ['220', '022', '000'],
+    ['020', '222', '000']
+  ]
+
+  function initialState() {
+    let myInitialPiece = {
+      pattern: ['010', '111', '000'],
+      global: { x: 0, y: 0 },
+      parts: [{ x: 0, y: 0 }]
+    }
+    let parts = []
+    myInitialPiece.pattern.forEach((e, i) => {
+      e.split('').map((el, il) => {
+        if (el == 1) {
+          parts.push({ x: myInitialPiece.global.x + il, y: myInitialPiece.global.y + i })
+        }
+      })
+    })
+
+    myInitialPiece.parts = parts
+    return myInitialPiece
+  }
+
+  function pieceGenerator(array) {
+    return array[Math.floor(Math.random() * 7)]
+  }
+
+  const [piece, setPiece] = useReducer(pieceReducer, initialState())
+
+
+
+  function handleKeyUp(e) {
+    if (e.code == 'ArrowDown') {
+      setSpeed(1000)
+    }
+  }
+
+  function patternReducer(state, action) {
+    switch (action.type) {
+      case 'rotate':
+        return state.map((e, i) => state.map((el) => el[(el.length - 1) - i]).join(''));
+      case 'rotate2':
+        return state.map((e, i) => state.map((el, y) => state[(state.length - 1) - y][i]).join(''));
+      // case 'reset':
+      //   return pieceGenerator(piecePattern);
+      default:
+        throw new Error();
+    }
+  }
+
+  function pieceReducer(state, action) {
+    let myPiece = [], newPattern
+    switch (action.type) {
+      case 'right':
+        if (state.parts.find(e => e.x == 9)) {
+          return { ...state }
+        }
+        return {
+          ...state,
+          global: { ...state.global, x: state.global.x++ },
+          parts: state.parts.map((e) => ({ ...e, x: e.x++ }))
+        };
+      case 'left':
+        if (state.parts.find(e => e.x == 0)) {
+          return { ...state }
+        }
+        return {
+          ...state,
+          global: { ...state.global, x: state.global.x-- },
+          parts: state.parts.map((e) => ({ ...e, x: e.x-- }))
+        };
+      case 'down':
+        if (state.parts.find(e => e.y == 19)) {
+          return { ...state }
+        }
+        return {
+          ...state,
+          global: { ...state.global, y: state.global.y++ },
+          parts: state.parts.map((e) => ({ ...e, y: e.y++ }))
+        };
+      case 'rotate':
+        newPattern = state.pattern.map((e, i) => state.pattern.map((el) => el[(el.length - 1) - i]).join(''));
+        newPattern.forEach((e, i) => {
+          e.split('').map((el, il) => {
+            if (el == 1) {
+              myPiece.push({ x: state.global.x + il, y: state.global.y + i })
+            }
+          })
+        })
+        return {
+          ...state,
+          pattern: newPattern,
+          parts: myPiece
+        }
+      case 'rotate2':
+        newPattern = state.pattern.map((e, i) => state.pattern.map((el, y) => state.pattern[(state.pattern.length - 1) - y][i]).join(''));
+        newPattern.forEach((e, i) => {
+          e.split('').map((el, il) => {
+            if (el == 1) {
+              myPiece.push({ x: state.global.x + il, y: state.global.y + i })
+            }
+          })
+        })
+        return {
+          ...state,
+          pattern: newPattern,
+          parts: myPiece
+        }
+      case 'reset':
+        return initialState();
+      default:
+        throw new Error();
+    }
+  }
+
+
+  function handleKeyDown(e) {
+    switch (e.code) {
+      case 'ArrowLeft':
+        setPiece({ type: 'left' })
+        break;
+      case 'ArrowRight':
+        setPiece({ type: 'right' })
+
+        break;
+      case 'ArrowDown':
+        if (!e.repeat) {
+          setSpeed(50)
+        }
+        break;
+      case 'ArrowUp':
+        setPiece({ type: 'rotate2' })
+        break;
+      case 'KeyW':
+        if (!e.repeat) {
+          setPiece({ type: 'rotate' })
+        }
+        break;
       default:
         break;
     }
   }
 
-  function handleKeyDown(e) {
 
-    const currBoard = [...baseBoard]
-
-    // rotation counter clockwise
-    if (e.code == "Space") {
-      const currPiece = [...piece]
-      const newPiece = [...piece]
-
-      for (let i = 0; i < currPiece.length; i++) {
-
-        // rotation center
-        if (i == 0) {
-          continue
-        }
-
-        // parts to rotate
-        let x = currPiece[0].x + (currPiece[i].y - currPiece[0].y)
-        let y = currPiece[0].y - (currPiece[i].x - currPiece[0].x)
-        newPiece[i].x = x
-        newPiece[i].y = y
-      }
-      setPiece(currPiece)
-    }
-
-    if (e.code == "ArrowDown") {
-    
-      setSpeed(50)
-    }
-
-    if (e.code == "ArrowLeft") {
+  /* delete lines  */
 
 
-      const currPiece = [...piece]
 
-      if (!currPiece.find(e => e.x == 0)) {
-        currPiece.forEach((e) => {
-          e.x--
-        })
-      }
-
-      setPiece(currPiece)
-    }
-
-    if (e.code == "ArrowRight") {
-      const currPiece = [...piece]
-
-      if (!currPiece.find(e => e.x == 9)) {
-        currPiece.forEach((e) => {
-          e.x++
-        })
-      }
-      setPiece(currPiece)
-    }
-  }
-
-  function handleKeyUp() {
-    setSpeed(1000)
-  }
-
-  /* delete lines  */ 
+  /* Render  */
   useEffect(() => {
+    let currBoard = [...baseBoard]
 
-    console.log('has lines')
-    const currBoard = [...baseBoard]
-    const tempBoard = [...currBoard]
-    currBoard.forEach((e, i) => {
-      if(e == '1111111111') {
-        tempBoard.splice(i, 1, '2222222222')
-      }
 
-      if(e == '2222222222') {
-        tempBoard.splice(i, 1)
-        tempBoard.unshift('0000000000')
-      }
-
+    if (piece.parts.find(e => e.y == 19) || piece.parts.find(e => currBoard[e.y + 1][e.x] == 1)) {
+      setBaseBoard(currBoard)
+      setPiece({ type: 'reset' })
+    }
+    console.log(piece.pattern)
+    piece.parts.forEach((e, i) => {
+      let cell = currBoard[e.y].split('')
+      cell[e.x] = 1
+      currBoard[e.y] = cell.join('')
     })
- 
-    setBaseBoard(tempBoard)
-    setIsLanded(false)
-  
-  }, [hasLines])
-
-
-  /* Render  */ 
-  useEffect(() => {
-
-    const currBoard = [...baseBoard]
-
-   
-    if(currBoard.includes('1111111111') || currBoard.includes('2222222222') ) {
-      setHasLines(hasLines + 1)
-    }
-  
-    
-    if (piece.find(e => e.y >= 19) || piece.find(e => currBoard[e.y][e.x] == 1)) {
-
-      setPiece(newPiece)
-      setBaseBoard(board)
-      setIsLanded(true)
-    }
-    else {
-      setIsLanded(false)
-      piece.forEach((e) => {
-        let cell = currBoard[e.y].split('')
-        cell[e.x] = 1
-        currBoard[e.y] = cell.join('')
-      })
-  
-      setBoard(currBoard)
-
-    }
-
+    setBoard(currBoard)
   }, [piece])
 
-  /* Controls  */ 
+
+
+  /* Controls  */
   useEffect(() => {
-
-    const currBoard = [...baseBoard]
-    window.addEventListener('keydown', handleKeyDown)
     window.addEventListener('keyup', handleKeyUp)
-
-    let interval = null
-    if (!isLanded) {
-
-      interval = setInterval(() => {
-        const currPiece = [...piece]
-        currPiece.forEach((e) => {
-          e.y++
-        })
-
-        setPiece(currPiece)
-      }, speed)
-
-    }
-
+    window.addEventListener('keydown', handleKeyDown)
+    let interval = setInterval(() => {
+      setPiece({ type: 'down' })
+    }, speed)
     return () => {
       clearInterval(interval);
+      window.removeEventListener('keyup', handleKeyUp);
       window.removeEventListener('keydown', handleKeyDown);
-      window.removeEventListener('keydown', handleKeyUp);
     }
+  }, [speed])
 
-  }, [isLanded, speed])
+
 
 
   return (
     <div className="app">
       {board.map((cell) => (
-        <div className="row">
-          {cell.split('').map((e) => <div className={`cell state${e}`}></div>)}
+        <div key={uuidv4()} className="row">
+          {cell.split('').map((e) => <div key={uuidv4()} className={`cell state${e}`}></div>)}
         </div>
       ))}
     </div>
---
+++ src/Appp.js
@@ -0,0 +1,229 @@
+import './App.css';
+import { useEffect, useState, useReducer } from 'react';
+
+function App() {
+
+  const [board, setBoard] = useState(new Array(31).fill('0000000000'))
+  const [piece, setPiece] = useState(newPiece)
+  const [baseBoard, setBaseBoard] = useState(new Array(31).fill('0000000000'))
+  const [isLanded, setIsLanded] = useState(false)
+  const [speed, setSpeed] = useState(1000)
+  const [hasLines, setHasLines] = useState(0)
+
+  const initialState = [
+    { x: 4, y: 0 },
+    { x: 4, y: 1 },
+    { x: 5, y: 0 },
+    { x: 3, y: 1 }
+  ];
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case 'down':
+        return state.map(e => ({x: e.x, y: y+1}));
+      case 'decrement':
+        return { count: state.count - 1 };
+      default:
+        throw new Error();
+    }
+  }
+
+  const [piece, dispatch] = useReducer(reducer, initialState);
+
+  function newPiece() {
+    let key = Math.floor(Math.random() * 5)
+    switch (key) {
+      case 0:
+        return [
+          { x: 4, y: 0 },
+          { x: 4, y: 1 },
+          { x: 5, y: 0 },
+          { x: 3, y: 1 }
+        ]
+      case 1:
+        return [
+          { x: 4, y: 0 },
+          { x: 5, y: 0 },
+          { x: 4, y: 1 },
+          { x: 5, y: 1 }
+        ]
+      case 2:
+        return [
+          { x: 4, y: 0 },
+          { x: 5, y: 0 },
+          { x: 3, y: 0 },
+          { x: 3, y: 1 }
+        ]
+      case 3:
+        return [
+          { x: 4, y: 0 },
+          { x: 5, y: 0 },
+          { x: 3, y: 0 },
+          { x: 4, y: 1 }
+        ]
+      case 4:
+        return [
+          { x: 4, y: 0 },
+          { x: 5, y: 0 },
+          { x: 3, y: 0 },
+          { x: 2, y: 0 }
+        ]
+      default:
+        break;
+    }
+  }
+
+  function handleKeyDown(e) {
+
+    const currBoard = [...baseBoard]
+
+    // rotation counter clockwise
+    if (e.code == "Space") {
+      const currPiece = [...piece]
+      const newPiece = [...piece]
+
+      for (let i = 0; i < currPiece.length; i++) {
+
+        // rotation center
+        if (i == 0) {
+          continue
+        }
+
+        // parts to rotate
+        let x = currPiece[0].x + (currPiece[i].y - currPiece[0].y)
+        let y = currPiece[0].y - (currPiece[i].x - currPiece[0].x)
+        newPiece[i].x = x
+        newPiece[i].y = y
+      }
+      setPiece(currPiece)
+    }
+
+    if (e.code == "ArrowDown") {
+
+      setSpeed(50)
+    }
+
+    if (e.code == "ArrowLeft") {
+
+
+      const currPiece = [...piece]
+
+      if (!currPiece.find(e => e.x == 0)) {
+        currPiece.forEach((e) => {
+          e.x--
+        })
+      }
+
+      setPiece(currPiece)
+    }
+
+    if (e.code == "ArrowRight") {
+      const currPiece = [...piece]
+
+      if (!currPiece.find(e => e.x == 9)) {
+        currPiece.forEach((e) => {
+          e.x++
+        })
+      }
+      setPiece(currPiece)
+    }
+  }
+
+  function handleKeyUp() {
+    setSpeed(1000)
+  }
+
+  /* delete lines  */
+  useEffect(() => {
+
+    console.log('has lines')
+    const currBoard = [...baseBoard]
+    const tempBoard = [...currBoard]
+    currBoard.forEach((e, i) => {
+      if (e == '1111111111') {
+        tempBoard.splice(i, 1, '2222222222')
+      }
+
+      if (e == '2222222222') {
+        tempBoard.splice(i, 1)
+        tempBoard.unshift('0000000000')
+      }
+
+    })
+
+    setBaseBoard(tempBoard)
+    setIsLanded(false)
+
+  }, [hasLines])
+
+
+  /* Render  */
+  useEffect(() => {
+
+    const currBoard = [...baseBoard]
+
+
+    if (currBoard.includes('1111111111') || currBoard.includes('2222222222')) {
+      setHasLines(hasLines + 1)
+    }
+
+
+    if (piece.find(e => e.y >= 19) || piece.find(e => currBoard[e.y][e.x] == 1)) {
+
+      setPiece(newPiece)
+      setBaseBoard(board)
+      setIsLanded(true)
+    }
+    else {
+      setIsLanded(false)
+
+      
+      piece.forEach((e) => {
+        let cell = currBoard[e.y].split('')
+        cell[e.x] = 1
+        currBoard[e.y] = cell.join('')
+      })
+
+      setBoard(currBoard)
+
+    }
+
+  }, [piece, baseBoard])
+
+  /* Controls  */
+  useEffect(() => {
+
+    const currBoard = [...baseBoard]
+    window.addEventListener('keydown', handleKeyDown)
+    window.addEventListener('keyup', handleKeyUp)
+
+    let interval = null
+    if (!isLanded) {
+
+      interval = setInterval(() => {
+        dispatch({type: 'down'})
+      }, speed)
+
+    }
+
+    return () => {
+      clearInterval(interval);
+      window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener('keydown', handleKeyUp);
+    }
+
+  }, [isLanded, speed])
+
+
+  return (
+    <div className="app">
+      {board.map((cell) => (
+        <div className="row">
+          {cell.split('').map((e) => <div className={`cell state${e}`}></div>)}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
